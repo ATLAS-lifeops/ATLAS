e1e2c3bb5733f9c28cfa70668d903fa0d1b79110
AlexToday111 <ernestkudakaev6@mail.ru>
2026-06-04 18:39:58 +0300

    fix(telegram): update panel logo asset path

--- src/main/java/com/example/atlas/telegram/TelegramMessageSender.java
+++ src/main/java/com/example/atlas/telegram/TelegramMessageSender.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class TelegramMessageSender {
 
     static final int MAX_TEXT_CHUNK_SIZE = 3900;
-    static final String ATLAS_PANEL_PHOTO = "https://raw.githubusercontent.com/ATLAS-lifeops/ATLAS/main/docs/assets/logo.png";
+    static final String ATLAS_PANEL_PHOTO = "https://raw.githubusercontent.com/ATLAS-lifeops/ATLAS/main/assets/logo.png";
 
     private static final Logger log = LoggerFactory.getLogger(TelegramMessageSender.class);
 
